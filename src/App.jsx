7d9930f944ac92705baece6f3972d4a1de8442f2
ApeWithGlasses <wcgiraldo@gmail.com>
2023-04-24 21:01:26 -0500

    Was change the card description

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
 export const cards = [
   {
     src: 'https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-    description: 'Hi, this are a description'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum arcu sed mi ultricies, et elementum magna tempor. Curabitur vel euismod est, nec blandit erat.'
   },
 ]
 
